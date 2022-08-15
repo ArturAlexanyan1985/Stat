fc959c715b364815c4a4cc68d1b90503fdf58f85
ArturAlexanyan1985 <artur_aleksanyan@inbox.ru>
2022-08-15 15:48:34 +0300

    First Commit

--- src/main/java/StatService.java
+++ src/main/java/StatService.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class StatService {
+import org.jetbrains.annotations.NotNull;
+
+public class StatService {
+    public long sum(long @NotNull [] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+    return sum;
+    }
+
+    public int maxSales(long @NotNull [] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public long average(long[] sales) {
+        return sum(sales) / 12;
+    }
+
+    public int avgLess(long[] sales) {
+        long avg = average(sales);
+        int count = 0;
+        for (long sale : sales ) {
+            if (sale < avg) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
+
+
+
+
--- src/test/java/StatServiceTest.java
+++ src/test/java/StatServiceTest.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class StatServiceTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+public class StatServiceTest {
+
+    private int expected;
+
+    @Test
+    public void testSum() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 19};
+        long actual = service.sum(sales);
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 19);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxSales() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 19};
+        long actual = service.maxSales(sales);
+        long expected = 6;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minSales() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 19};
+        long actual = service.minSales(sales);
+        long expected = 6;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void average() {
+        StatService service = new StatService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 19};
+        long actual = service.average(sales);
+        long expected = 15;
+
+        assertEquals(expected, actual);
+    }
+
 }
